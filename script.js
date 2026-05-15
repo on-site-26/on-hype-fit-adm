305de1b8ce9a39f5cff965c2f51a48a93f5547ac
on-site-26 <starmidias07@gmail.com>
2026-05-15 02:25:56 -0300

    Update script.js
    1.6

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
 // Inicializa o Firebase e força a conexão com o Banco de Dados Online
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-    };
     
     Chart.register(ChartDataLabels);
 
